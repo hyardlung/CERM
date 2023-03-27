24e92db581fc43ba6021d83b595427f09e1aafec
Dmitry Volkov <hyardlung@gmail.com>
2023-03-27 11:52:24 +0700

    init

---
+++ src/main.js
@@ -0,0 +1,42 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp, provide, h } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://rickandmortyapi.com/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+
+registerPlugins(app)
+
+app.mount('#app')
